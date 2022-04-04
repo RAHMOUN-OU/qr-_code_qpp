ee8eeee91784d9466008dd7e36e58a4b864ea445
rahmoun oussama <mansiti777oussama@gmail.com>
2022-04-04 14:20:32 +0200

    inisialisation first hosting in github

---
+++ jss.js
@@ -0,0 +1,13 @@
+const wrapper=document.querySelector(".wrapper");
+gen_btn=wrapper.querySelector(".wrapper .form button");
+qr_in=wrapper.querySelector(".form input");
+qr_img=wrapper.querySelector(".qr_code img");
+
+gen_btn.addEventListener("click",() => {
+    let qr_value=qr_in.value;
+    if(!qr_value) return;
+    qr_img.src='https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qr_value}';
+
+wrapper.classList.add("action");
+
+})
